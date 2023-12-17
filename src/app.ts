10cc9debd04c700a3f45ec4cc7aa0e346bbcc3fa
monir178 <monir.mzs17@gmail.com>
2023-12-17 16:24:01 +0600

    [Done] create student feature

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
 const app: Application = express()
-// const port = 3000;
 
 //parsers
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 4
+//application routes
+app.use('/api/v1/students', StudentRoutes)
 
+const getAController = (req: Request, res: Response) => {
+  const a = 10
   res.send(a)
-})
+}
+app.get('/', getAController)
 
 export default app
---
+++ src/app/modules/student/student.route.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { studentControllers } from './student.controller'
+
+const router = express.Router()
+
+//will call controller function
+router.post('/create-student', studentControllers.createStudent)
+
+router.get('/', studentControllers.getAllStudents)
+
+router.get('/:studentId', studentControllers.getSingleStudent)
+
+export const StudentRoutes = router
